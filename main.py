482ced11703f14d307368ae4c226d06fb5c3e588
ROBINSON ENEDINO E SILVA <robinsonbrz@gmail.com>
2022-05-20 21:32:20 -0300

    👌 refatorado

--- main.py
+++ main.py
@@ -7,6 +7,14 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+
+def usuario_existe(id_usuario):
+    for usuario in base_de_dados:
+        if(usuario.id == id_usuario):
+            return usuario
+    return {"Status":404, "Mensagem": "Não encontrou usuario"}
+
+
 app = FastAPI()
 
 # rota Raiz
@@ -36,23 +44,25 @@ def get_todos_os_usuarios():
 # Rota Get Id
 @app.get("/usuarios/{id_usuario}")
 def get_usuario_usando_id(id_usuario: int): # definir o tipo para não haver erro
-    for usuario in base_de_dados:
-        if(usuario.id == id_usuario):
-            return usuario
-    return {"Status":404, "Mensagem": "Não encontrou usuario"}
+    return usuario_existe(id_usuario)
 
 # Rota Insere
 @app.post("/usuarios")
 def insere_usuario(usuario: Usuario):
     # importante criar regras de negocio ex: duplicidade
+    print(type(usuario))
     base_de_dados.append(usuario)
     return usuario
 
 # Rota Delete Id
 @app.delete("/usuarios/{id_usuario}")
 def delete_item(id_usuario: int):
-    for usuario in base_de_dados:
-        if(usuario.id == id_usuario):
-            base_de_dados.remove(usuario)
-            return {'Message': 'Item deleted successfully'}
-    return {"Status":404, "Mensagem": "Não encontrou usuario para deletar"}
+    usuario = usuario_existe(id_usuario)
+    if isinstance(usuario, Usuario):
+        base_de_dados.remove(usuario)
+        return {'Message': f'Usuário id: {usuario.id} deletado'}
+    return usuario
+
+
+# Rota Edit Id
+
